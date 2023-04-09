442dcdab8312b074e2e99a8cf03ad7a80e562a25
TekniqueJoseph <joejacksun@gmail.com>
2023-04-09 10:55:04 -0400

    updated code notation - spread operator

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// Object Spread Operator
+// Spread Operator - With array
+const array = [1,2,3,4,5,];
+const sum = ((a,b,c,d,e) =>{
+    return a+b+c+d+e;
+})
+console.log(sum(...array))
 
+// Spread Operator - With Object
 const animals = {
     tiger: 23,
     lion: 5,
     monkey: 2,
     bird: 40
 }
-
-const array = [1,2,3,4,5,];
-
-const sum = ((a,b,c,d,e) =>{
-    return a+b+c+d+e;
-})
-console.log(sum(...array))
 
 const {tiger, lion,...rest} = animals; 
 console.log(tiger)
